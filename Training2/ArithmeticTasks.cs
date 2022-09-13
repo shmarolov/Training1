d33af9f39e7e2a3a87e5b2a2e408018f4512e05b
shmarolov <beglanesirkepov@gmail.com>
2022-09-13 19:50:14 +0600

    Добавьте файлы проекта.

---
+++ Training2/ArithmeticTasks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Training
+{
+    internal class ArithmeticTasks
+    {
+        static (int x, int y) Method((int x, int y) tuple)
+        {
+            var res = (x: tuple.y, y: tuple.x);
+            return res;
+        }
+    }
+}
---
+++ Training2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Training
+{
+    class Train : ArithmeticTasks
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            List<string> answers = new List<string>();
+            answers.Add(ArithmeticTasks.Method(x, y)=(10, 5).ToString());
+            foreach (var answer in answers)
+            {
+                counter++;
+
+                Console.WriteLine($"1.{counter}. x={answer.x}, y={answers.y}");
+            }
+        }
+    }
+}
